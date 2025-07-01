f3b7ef7586fcf2ed00c94efc018fde765958460b
trivedi089 <satyachbs007@gmail.com>
2025-07-01 23:17:30 +0530

    Sorting Techniques

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -1,4 +1,37 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class LinkedList {
+
+    public class Node{}{
+        int data;
+        Node next;
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+        System.out.println("Choose wha you want to do : /n" +
+                "1. Insertion" +
+                "2. Deletion" +
+                "3. Merge in between");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        if (choice == 1) {
+            System.out.print("How many elements do you want to insert : ");
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int element = sc.nextInt();
+                insertElement(list, element)
+            }
+        }
+
+    }
+
+    static void insertElement(LinkedList list, int element){
+        Node newNode = new Node(element,null);
+        list->Node=newNode;
+    }
+
 }
